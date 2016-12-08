60295f068ef272550328ffd5a38cbaa94565e351
enrico.campagnaro@studenti.unitn.it <enrico.campagnaro@studenti.unitn.it>
2016-12-08 10:46:24 +0100

    Ho terminato alcune modifiche

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ var util = require('util');
 //post
 var bind = require('bind');
 
+var db = require('./database.js')
+
+var HTML = require('./createHTML.js')
+
+
+var session = require('express-session')
+app.use(session({  //per settare la libreria delle sessioni
+	//required, used to prevent tampering
+	secret: 'stringa per la sessione',   //propria stringa che permette di non far modificare il cookie della sessione
+	//set time of validity of cookies
+	cookie: { maxAge: 60000 } //durata massima in millisecondi
+}));
+
+
 app.get('/stile.css', function(req,res){
 	res.sendFile('template/stile.css' , { root : __dirname });
 });
@@ -16,135 +30,87 @@ app.get('/script.js', function(req,res){
 	res.sendFile('template/script.js' , { root : __dirname });
 });
 
-var connectionString = process.env.DATABASE_URL || 
-    'postgres://usnbjcwzevjnge:4uObCrPpTSW3xSGc5E5hgp23IO@ec2-54-247-81-123.eu-west-1.compute.amazonaws.com:5432/d4mmdqep139jq1'
     
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
     bind.toFile('template/homepage.html',{}, 
     function(data) 
     {
         //write resonse
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(data);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
     });
 });
 
-//get
-app.get('/caricagiorni', function(request, response) {
-    bind.toFile('template/selezione_giorni.html',{}, 
+
+app.get('/caricagiorni', function(req,res){
+	
+	db.loadQuery({
+		text: 'select data from GiorniOrdinazioni',
+	}, function(err,giorni) {
+		if(err){
+		  res.redirect('/error');
+		}
+		else {
+          
+          for(a in giorni){
+            var d = new Date(giorni[a].data);
+            var str = $.datepicker.formatDate('yy-mm-dd', d);
+            console.log("giorni:" + str);
+          }
+          var tbody = HTML.creaTbodyGiorni();
+          //creare tbody con i giorni presi dal database
+          
+          bind.toFile('template/selezione_giorni.html',{
+              tbody: tbody
+          },function(data){
+              res.writeHead(200, {'Content-Type':'text/html'});
+              res.end(data);
+          });
+		}
+    });
+});		
+
+
+
+app.get('/caricapasti', function(req,res){
+	/*
+	db.loadQuery({
+		text: 'select data from GiorniOrdinazioni',
+	}, function(err,giorni) {
+		if(err){
+		  res.redirect('/error');
+		}
+		else {
+          */
+          var tbody = "<tbody><tr><td>10-12-1555555555</td><td><input type=\"checkbox\"></td></tr><tr><td>11-12-15</td><td><input type=\"checkbox\"></td></tr><tr><td>12-12-15</td><td><input type=\"checkbox\"></td></tr></tbody>"
+          //creare tbody con i giorni presi dal database
+          
+          bind.toFile('template/selezione_pasti.html',{
+              tbody: tbody
+          },function(data){
+              res.writeHead(200, {'Content-Type':'text/html'});
+              res.end(data);
+          });
+		});
+        /*
+    });
+});		
+*/
+
+
+app.get('/error', function(req, res) {
+    bind.toFile('template/error.html',{}, 
     function(data) 
     {
         //write resonse
-        console.log(data);
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(data);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
     });
 });
 
-
-/**
- * @brief run select query
- * @return content of table test_table
- */
-app.get('/select/', function(request, response) 
-{
-	var text = 'responce:';
-	response.writeHead(200, {'Content-Type': 'text/html'});
-    
-    console.log(connectionString);
-
-	//connect to database
-	pg.connect(
-		//enviromental variable, set by heroku when first databse is created
-		connectionString, 
-		function(err, client, done) {
-		//query
-		client.query('SELECT * FROM test_table', function(err, result) {
-			//release the client back to the pool
-			done();
-			
-			//manages err
-			if (err){ 
-				console.error(err); 
-				response.end("Error select" + err); 
-		  	}
-		  	else {
-				text = "<p>Dump db: <br> " + util.inspect(result.rows) + ".</p>";
-				text = text + "<br> <br>";
-		  	}
-			
-			//response here, otherwise the page will be sent before the execution of the query
-			console.log("text final: "+text);
-			response.end(text);
-		});
-  	});
-
-});
-
-/**
- * @brief run select query
- * @return notification or error
- */
-app.get('/create/', function(request, response) 
-{
-	var text = 'responce:';
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	
-	console.log("called");
-
-	pg.connect(connectionString, function(err, client, done) {
-		
-		console.log("connected to db");
-	
-		//create table	
-		client.query('create table test_table (id integer, name text)', function(err, result) {
-		  done();
-			
-		  if (err){ 
-			   console.error(err); 
-			   response.send("Error " + err); 
-		   }
-		  else{ 
-			  response.end("table created");
-		   }
-		});
-
-  	});
-  	
-
-});
-
-/**
- * @brief run add query
- * @return notification or error
- */
-app.get('/add/', function(request, response) 
-{
-	var text = 'responce:';
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	
-	console.log("called");
-
-	pg.connect(connectionString, function(err, client, done) {
-		
-		console.log("connected to db");
-
-		//add element
-		client.query('insert into test_table values (1, \'hello database\')', function(err, result) {
-		  done();
-		  if (err) { 
-			  console.error(err); 
-			  response.send("Error insert " + err); }
-		  else {
-			  response.end("row added");
-		   }
-		});
-  	});
-
-});
 
 
 app.listen(app.get('port'), function() {
--- template/script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){//get
-	$("#ordinazione").click(function(){
-		$.get("http://127.0.0.1:5000/caricagiorni");
-	});
-	
-});

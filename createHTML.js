17263614b9390d7d9de53f46e8bdf404d9429482
enrico.campagnaro@studenti.unitn.it <enrico.campagnaro@studenti.unitn.it>
2016-12-09 17:20:34 +0100

    tolto una parte inutile

--- createHTML.js
+++ createHTML.js
@@ -47,36 +47,7 @@ function creaTabellaOrdinazioniPossibili(ord){
   text += "</tbody>\n";
   return text;
 }
-/*
 
-<tbody>
-    <tr>
-      <td>data</td>
-      <td>Pasta<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-      <td>carne<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-      <td>spinaci<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-     <td>frutta<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-     </tr>
-    <tr>
-        <td><b>Secondo</b></td>
-        <td>Carne<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Pesce<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Carne<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-    </tr>
-    <tr>
-        <td><b>Contorno</b></td>
-        <td>Patate<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Piselli<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Carote<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-    </tr>
-    <tr>
-        <td><b>Dessert</b></td>
-        <td>Torta della nonna<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Frutta<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-        <td>Frutta<input type="checkbox"><button class="btn-info"><span class="glyphicon glyphicon-info-sign"></span></button></td>
-    </tr>
-</tbody>
-*/
 
 exports.creaTbodyGiorni = creaTbodyGiorni; 
 exports.creaTabellaOrdinazioniPossibili = creaTabellaOrdinazioniPossibili; 
